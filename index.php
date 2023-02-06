5cc08c25c01e71394e878f66b411700230a6ad32
GiaaHuyy0112 <huynguyen@coffeemugvn.com>
2023-02-07 01:27:32 +0700

    Add

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <meta charset="UTF-8" />
+  <title>Valentine</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="/css/app.css" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+  <script src="/js/app.js"></script>
+  <body>
+  <div id="header-plugin"></div>
+
+<div class="bgoverlay">
+  <div class="container">
+    <span class="ico">
+      <span class="ico2"></span>
+      <span class="title">Click Me</span>
+    </span>
+    <div class="endtext">
+      <span class="close" title="Restart"><i class="fa fa-times"></i></span>  
+      <h1>Xin chào Thảo Ly xinh đẹp</h1>
+      <h2>Chúc em Valentine vui vẻ. Đọc tới đây thì em check mail được rồi đó :D</h2>
+      <h3>- Gia Huy</h3>
+    </div>
+  </div>
+</div>
+
+  </body>
+</html>
